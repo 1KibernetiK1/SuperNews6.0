ca6e8015fba803ca28f0e15962a4b5bf400d505e
1KibernetiK1 <123451040o@gmail.com>
2023-03-28 20:49:10 +0500

    Поправки в header

--- SuperNews/Program.cs
+++ SuperNews/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDistributedMemoryCache();
